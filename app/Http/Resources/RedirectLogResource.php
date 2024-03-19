5f32640dd42522ac2c66db52dd29f22fa66611dd
Clebson Moura <clebson.sh@gmail.com>
2024-03-19 15:17:38 -0300

    Add RedirectLogResource

---
+++ app/Http/Resources/RedirectLogResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RedirectLogResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'redirect_code' => $this->redirect->code,
+            'ip_address' => $this->ip_address,
+            'user_agent' => $this->user_agent,
+            'referer' => $this->referer,
+            'query_params' => $this->query_params,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
