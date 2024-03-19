267c8511b71efc60432db52310eb53cc2174a3fd
Clebson Moura <clebson.sh@gmail.com>
2024-03-18 23:18:14 -0300

    Add RedirectController for API endpoints

---
+++ app/Http/Controllers/Api/RedirectController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRedirectRequest;
+use App\Http\Requests\UpdateRedirectRequest;
+use App\Http\Resources\RedirectResource;
+use App\Models\Redirect;
+use Illuminate\Http\Response;
+use Vinkla\Hashids\Facades\Hashids;
+
+class RedirectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return RedirectResource::collection(Redirect::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRedirectRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRedirectRequest $request)
+    {
+        $validated = $request->validated();
+
+        Redirect::create($validated);
+
+        return response()->json(['message' => 'Redirect created'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRedirectRequest  $request
+     * @param  \App\Models\Redirect  $redirect
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRedirectRequest $request, string $code)
+    {
+        $validated = $request->validated();
+
+        $redirect = Redirect::where('id', Hashids::decode($code))->firstOrFail();
+
+        $redirect->update($validated);
+
+        return response()->json(['message' => 'Redirect updated'], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Redirect  $redirect
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(string $code)
+    {
+        $redirect = Redirect::where('id', Hashids::decode($code))->firstOrFail();
+
+        $redirect->update(['status' => 'inactive']);
+
+        $redirect->delete();
+
+        return response()->json(['message' => 'Redirect deleted'], Response::HTTP_OK);
+    }
+}
