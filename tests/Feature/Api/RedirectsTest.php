a23b1b2a9075f75a7a8498630dfceb626e7f71dd
Clebson Moura <clebson.sh@gmail.com>
2024-03-21 12:01:37 -0300

    Add tests for creating and updating redirects with valid URLs and query parameters

--- tests/Feature/Api/RedirectsTest.php
+++ tests/Feature/Api/RedirectsTest.php
@@ -28,6 +28,58 @@ class RedirectsTest extends TestCase
         $this->assertCount(1, Redirect::all());
     }
 
+    public function test_create_redirect_with_valid_url_with_query_params()
+    {
+        $response = $this->post(route('redirects.store'), [
+            'url' => 'https://www.google.com?test=1&test2=2',
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJsonPath('message', 'Redirect created');
+
+        $this->assertCount(1, Redirect::all());
+    }
+
+    public function test_update_redirect_with_valid_url()
+    {
+        $url = 'https://www.google.com';
+        $redirect = Redirect::factory()->create(['url' => 'https://www.example.com']);
+
+        $response = $this->put(route('redirects.update', $redirect->code), [
+            'url' => $url,
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonPath('message', 'Redirect updated');
+
+        $redirects = Redirect::all();
+        $this->assertCount(1, $redirects);
+        $this->assertEquals($url, $redirects->first()->url);
+    }
+
+    public function test_update_redirect_with_valid_url_with_query_params()
+    {
+        $url = 'https://www.google.com?test=1&test2=2';
+        $redirect = Redirect::factory()->create(['url' => 'https://www.example.com']);
+
+        $response = $this->put(route('redirects.update', $redirect->code), [
+            'url' => $url,
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonPath('message', 'Redirect updated');
+
+        $redirects = Redirect::all();
+        $this->assertCount(1, $redirects);
+        $this->assertEquals($url, $redirects->first()->url);
+    }
+
     public function test_fail_to_create_redirect_with_invalid_url()
     {
         $response = $this->post(route('redirects.store'), [
@@ -84,23 +136,18 @@ class RedirectsTest extends TestCase
         $this->assertCount(0, Redirect::all());
     }
 
-    public function test_update_redirect_with_valid_url()
+    public function test_fail_to_create_redirect_with_null_query_params()
     {
-        $url = 'https://www.google.com';
-        $redirect = Redirect::factory()->create(['url' => 'https://www.example.com']);
-
-        $response = $this->put(route('redirects.update', $redirect->code), [
-            'url' => $url,
+        $response = $this->post(route('redirects.store'), [
+            'url' => 'https://www.google.com?param1=value1&param2=',
         ], [
             'Accept' => 'application/json',
         ]);
 
-        $response->assertStatus(200);
-        $response->assertJsonPath('message', 'Redirect updated');
+        $response->assertStatus(422);
+        $response->assertJsonPath('message', 'The URL must not have null query parameters.');
 
-        $redirects = Redirect::all();
-        $this->assertCount(1, $redirects);
-        $this->assertEquals($url, $redirects->first()->url);
+        $this->assertCount(0, Redirect::all());
     }
 
     public function test_fail_to_update_redirect_with_invalid_url()
@@ -115,6 +162,70 @@ class RedirectsTest extends TestCase
 
         $response->assertStatus(422);
         $response->assertJsonPath('message', 'The URL is not valid.');
+
+        $this->assertCount(1, Redirect::all());
+    }
+
+    public function test_fail_to_update_redirect_with_app_url()
+    {
+        $redirect = Redirect::factory()->create(['url' => 'https://www.example.com']);
+
+        $response = $this->put(route('redirects.update', $redirect->code), [
+            'url' => config('app.url'),
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonPath('message', 'The URL must be different from the server URL.');
+
+        $this->assertCount(1, Redirect::all());
+    }
+
+    public function test_fail_to_update_redirect_with_non_https_url()
+    {
+        $redirect = Redirect::factory()->create(['url' => 'https://www.example.com']);
+
+        $response = $this->put(route('redirects.update', $redirect->code), [
+            'url' => 'http://www.google.com',
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonPath('message', 'The URL must use the HTTPS protocol.');
+
+        $this->assertCount(1, Redirect::all());
+    }
+
+    public function test_fail_to_update_redirect_with_offline_url()
+    {
+        $redirect = Redirect::factory()->create(['url' => 'https://www.example.com']);
+
+        $response = $this->put(route('redirects.update', $redirect->code), [
+            'url' => 'https://www.google.com/not-found',
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonPath('message', 'The URL must be online.');
+
+        $this->assertCount(1, Redirect::all());
+    }
+
+    public function test_fail_to_update_redirect_with_null_query_params()
+    {
+        $redirect = Redirect::factory()->create(['url' => 'https://www.example.com']);
+
+        $response = $this->put(route('redirects.update', $redirect->code), [
+            'url' => 'https://www.google.com?param1=value1&param2=',
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonPath('message', 'The URL must not have null query parameters.');
 
         $this->assertCount(1, Redirect::all());
     }
